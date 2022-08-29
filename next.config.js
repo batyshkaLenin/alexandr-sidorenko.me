515b5d8339391810836fffdd673fa095e6a92781
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-30 04:53:19 +0700

    Добавлен PWA и GA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-module.exports = {
+const withPWA = require("next-pwa")
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  dest: "public",
+  publicExcludes: ['!rss/*', '!sitemap.xml', '!sitemap-*.xml'],
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  runtimeCaching,
+})({
   reactStrictMode: true,
   serverRuntimeConfig: {
+    GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
     YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
     HOST: process.env.NEXT_PUBLIC_HOST,
   },
   publicRuntimeConfig: {
+    GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
     YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
     HOST: process.env.NEXT_PUBLIC_HOST,
   },
-};
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,11 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Router from 'next/router'
 import {useAmp} from "next/amp";
+import Script from "next/script";
 
 const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
+const isServerSide = typeof window === 'undefined'
+const { YM_CODE, GA_CODE } = isServerSide ? serverRuntimeConfig : publicRuntimeConfig
 
 function _App({ Component, pageProps }) {
   const isAmp = useAmp()
@@ -31,14 +34,13 @@ function _App({ Component, pageProps }) {
         </main>
         <Footer />
       </div>
+       <Script id='ext-ga' async src={`https://www.googletagmanager.com/gtag/js?id=${GA_CODE}`} strategy="afterInteractive" />
+       <Script id='int-ga' strategy="afterInteractive">{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', ${GA_CODE});`}</Script>
     </>
   )
 }
 
 _App.displayName = 'App'
-
-const isServerSide = typeof window === 'undefined'
-const { YM_CODE } = isServerSide ? serverRuntimeConfig : publicRuntimeConfig
 
 const App = withYM(YM_CODE, Router)(_App)
 
