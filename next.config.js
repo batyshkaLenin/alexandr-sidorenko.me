394864a054fd13084c0b92569484da2f92c43a72
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-29 08:00:17 +0700

    Удалил экспериментальную фичу

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   ignoreDevErrors: false,
-  experimental: {
-    scrollRestoration: true,
-  },
   serverRuntimeConfig: {
     YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
     HOST: process.env.NEXT_PUBLIC_HOST,
--- pages/creation/index.tsx
+++ pages/creation/index.tsx
@@ -4,7 +4,7 @@ import Helmet from "../../components/Helmet";
 export const config = { amp: 'hybrid' }
 
 const CreationPage: NextPage = () => (<>
-    <Helmet title='Творчество Александра Сидоренко' />
+    <Helmet title='Творчество Александра Сидоренко' description="Страница с публикациями моего творчества. Я занимаюсь музыкой, пишу стихи и рассказы." />
     <div>Страница находится в разработке</div>
 </>)
 
