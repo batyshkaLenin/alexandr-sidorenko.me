e962513a3c4f8f94241462f0ab19d4bfdf1e6e8d
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-30 05:16:10 +0700

    Оптимизация Web-Vitals

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main } from 'next/document'
 import { FC } from 'react'
+import DeferNextScript from "../components/DeferNextScript";
 
 const Document: FC = () => (
   <Html lang='ru'>
     <Head />
     <body>
       <Main />
-      <NextScript />
+      <DeferNextScript />
     </body>
   </Html>
 )
