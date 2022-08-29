8f854ca5b5c6c2c904bba1ea61564447cd3e1f0f
Alexandr Sidorenko <auroragamesproject@gmail.com>
2022-08-29 08:22:26 +0700

    Поправил описание на странице Творчество

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  ignoreDevErrors: false,
   serverRuntimeConfig: {
     YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
     HOST: process.env.NEXT_PUBLIC_HOST,
