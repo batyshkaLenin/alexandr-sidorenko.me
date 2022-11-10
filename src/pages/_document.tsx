32820209b007c812d8de661206b088248ae43adf
batyshkaLenin <auroragamesproject@gmail.com>
2022-11-10 15:18:26 +0600

    Added vercel analytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import { Head, Html, Main } from 'next/document'
 import { FC } from 'react'
 import DeferNextScript from "../components/DeferNextScript";
 import useTranslation from "../lib/hooks/useTranslation";
+import { Analytics } from '@vercel/analytics/react';
 
 const Document: FC = () => {
     const { locale } = useTranslation()
@@ -12,6 +13,7 @@ const Document: FC = () => {
             <body data-ssml-voice-gender="male">
             <Main />
             <DeferNextScript />
+            <Analytics />
             </body>
         </Html>
     )
