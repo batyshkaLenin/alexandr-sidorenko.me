9c76e9880e8635770572c7ec797c08dc4e3cd1d1
batyshkaLenin <auroragamesproject@gmail.com>
2022-11-10 06:52:34 +0600

    Added alternate locale link

--- src/components/Helmet/index.tsx
+++ src/components/Helmet/index.tsx
@@ -35,6 +35,7 @@ const Helmet = ({ title, description, keywords, image, children }: HelmetProps) 
 
             {/* Basic */}
             <link href={canonicalUrl} rel='canonical' />
+            <link rel="alternate" hrefLang="en" href={`${canonicalUrl}en`} />
             <meta content={customTitle} name='title' />
             <meta content={customDescription} name='description' />
             <meta content={customKeywords} name='keywords' />
