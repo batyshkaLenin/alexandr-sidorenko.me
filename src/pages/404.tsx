8ca981eba4578ce1c4ce8e5781a850354fb6bf25
batyshkaLenin <auroragamesproject@gmail.com>
2022-11-11 02:31:48 +0600

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import Helmet from '../components/Helmet'
+import locales from '../lib/locales'
+import { Locale } from '../lib/types'
+import styles from '../styles/Error.module.scss'
+
+const NotFoundPage: NextPage<{ locale: Locale }> = ({ locale }) => (
+  <>
+    <Helmet title={locales[locale]['NOT_FOUND']}>
+      <meta content='noindex,nofollow,noarchive' name='robots' />
+    </Helmet>
+    <div className={styles.errorBlock}>
+      <span>{locales[locale]['NOT_FOUND']}</span>
+      <Link href='/'>
+        <a>{locales[locale]['BACK_TO_MAIN_PAGE']}</a>
+      </Link>
+    </div>
+  </>
+)
+
+NotFoundPage.getInitialProps = ({ locale }) => ({ locale: locale as Locale })
+
+export default NotFoundPage
