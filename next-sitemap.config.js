bbc7d61cc68c0ee3d9982447053ff0313d7b5754
batyshkaLenin <auroragamesproject@gmail.com>
2022-11-11 15:52:44 +0600

    Remove 404 from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,16 @@ const siteURL = process.env.NEXT_PUBLIC_HOST || 'https://alexandr-sidorenko.me';
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: siteURL,
+  exclude: ['/404', '/en/404'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/404', '/en/404'],
+      },
+      { userAgent: '*', allow: '/' },
+    ],
+  },
   generateRobotsTxt: true,
   optimizeFonts: true,
   devBrowserSourceMaps: true,
