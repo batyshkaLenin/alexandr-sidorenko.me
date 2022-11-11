7d6f43c2b44a3e5a0b3a82abcc7945be83247634
batyshkaLenin <auroragamesproject@gmail.com>
2022-11-11 16:32:10 +0600

    Set sentry silent

--- next.config.js
+++ next.config.js
@@ -13,107 +13,111 @@ module.exports = withPWA({
   cacheOnFrontEndNav: true,
   runtimeCaching,
 })(
-  withSentryConfig({
-    reactStrictMode: true,
-    experimental: {
-      amp: {
-        skipValidation: true,
+  withSentryConfig(
+    {
+      reactStrictMode: true,
+      experimental: {
+        forceSwcTransforms: true,
+        amp: {
+          skipValidation: true,
+        },
+      },
+      sentry: {
+        hideSourceMaps: true,
+      },
+      images: {
+        domains: ['webring.xxiivv.com'],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy:
+          "default-src 'self'; script-src 'none'; sandbox;",
+      },
+      i18n: {
+        locales: ['en', 'ru'],
+        defaultLocale: 'ru',
+      },
+      async redirects() {
+        return [
+          {
+            source: '/post/:slug',
+            destination: '/posts/:slug',
+            permanent: true,
+          },
+          {
+            source: '/blog/:slug',
+            destination: '/posts/:slug',
+            permanent: true,
+          },
+          {
+            source: '/blog',
+            destination: '/posts',
+            permanent: true,
+          },
+          {
+            source: '/music',
+            destination: '/creativity',
+            permanent: true,
+          },
+          {
+            source: '/creation',
+            destination: '/creativity',
+            permanent: true,
+          },
+          {
+            source: '/creation/:slug',
+            destination: '/creativity/:slug',
+            permanent: true,
+          },
+          {
+            source: '/assets/creation/:path*',
+            destination: '/assets/creativity/:path*',
+            permanent: true,
+          },
+          {
+            source: '/rss/creation/feed.xml',
+            destination: '/rss/ru/creativity/feed.xml',
+            permanent: true,
+          },
+          {
+            source: '/rss/posts/feed.xml',
+            destination: '/rss/ru/posts/feed.xml',
+            permanent: true,
+          },
+          {
+            source: '/rss/creation/feed.json',
+            destination: '/rss/ru/creativity/feed.json',
+            permanent: true,
+          },
+          {
+            source: '/rss/posts/feed.json',
+            destination: '/rss/ru/posts/feed.json',
+            permanent: true,
+          },
+        ]
+      },
+      webpack: (config, { dev, isServer }) => {
+        if (dev && !isServer) {
+          config.plugins.push(new ForkTsCheckerWebpackPlugin())
+        }
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+        }
+
+        return config
+      },
+      serverRuntimeConfig: {
+        GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
+        YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
+        HOST: process.env.NEXT_PUBLIC_HOST,
+      },
+      publicRuntimeConfig: {
+        GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
+        YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
+        HOST: process.env.NEXT_PUBLIC_HOST,
       },
     },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'webring.xxiivv.com',
-          port: '',
-        },
-      ],
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    {
+      silent: true,
     },
-    i18n: {
-      locales: ['en', 'ru'],
-      defaultLocale: 'ru',
-    },
-    async redirects() {
-      return [
-        {
-          source: '/post/:slug',
-          destination: '/posts/:slug',
-          permanent: true,
-        },
-        {
-          source: '/blog/:slug',
-          destination: '/posts/:slug',
-          permanent: true,
-        },
-        {
-          source: '/blog',
-          destination: '/posts',
-          permanent: true,
-        },
-        {
-          source: '/music',
-          destination: '/creativity',
-          permanent: true,
-        },
-        {
-          source: '/creation',
-          destination: '/creativity',
-          permanent: true,
-        },
-        {
-          source: '/creation/:slug',
-          destination: '/creativity/:slug',
-          permanent: true,
-        },
-        {
-          source: '/assets/creation/:path*',
-          destination: '/assets/creativity/:path*',
-          permanent: true,
-        },
-        {
-          source: '/rss/creation/feed.xml',
-          destination: '/rss/ru/creativity/feed.xml',
-          permanent: true,
-        },
-        {
-          source: '/rss/posts/feed.xml',
-          destination: '/rss/ru/posts/feed.xml',
-          permanent: true,
-        },
-        {
-          source: '/rss/creation/feed.json',
-          destination: '/rss/ru/creativity/feed.json',
-          permanent: true,
-        },
-        {
-          source: '/rss/posts/feed.json',
-          destination: '/rss/ru/posts/feed.json',
-          permanent: true,
-        },
-      ]
-    },
-    webpack: (config, { dev, isServer }) => {
-      if (dev && !isServer) {
-        config.plugins.push(new ForkTsCheckerWebpackPlugin())
-      }
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      }
-
-      return config
-    },
-    serverRuntimeConfig: {
-      GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
-      YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
-      HOST: process.env.NEXT_PUBLIC_HOST,
-    },
-    publicRuntimeConfig: {
-      GA_CODE: process.env.NEXT_PUBLIC_GA_CODE,
-      YM_CODE: process.env.NEXT_PUBLIC_YM_CODE,
-      HOST: process.env.NEXT_PUBLIC_HOST,
-    },
-  }),
+  ),
 )
